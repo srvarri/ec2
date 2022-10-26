b80a8acf7bbb8fbd73b323b23541f0d096b61a55
unknown <srvarri@gmail.com>
2022-10-27 00:27:04 +0530

    added

---
+++ var.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#******************************************************************************
+#    AWS VPC Creation Shell Script
+#******************************************************************************
+#==============================================================================
+#   MODIFY THE SETTINGS BELOW
+#==============================================================================
+AWS_REGION="us-west-1"
+VPC_NAME="My VPC"
+VPC_CIDR="10.0.0.0/16"
+#==============================================================================
+#   DO NOT MODIFY CODE BELOW
+#==============================================================================
+# Create VPC
+echo "Creating VPC in preferred region..."
+VPC_ID=$(aws ec2 create-vpc \
+  --cidr-block $VPC_CIDR \
+  --query 'Vpc.{VpcId:VpcId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  VPC ID '$VPC_ID' CREATED in '$AWS_REGION' region."
