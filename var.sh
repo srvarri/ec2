069b5c7b25bdd61ec266b89fcc8e68c4a7a7d9ec
unknown <srvarri@gmail.com>
2022-10-27 00:47:12 +0530

    added

--- var.sh
+++ var.sh
@@ -8,6 +8,9 @@
 AWS_REGION="us-west-1"
 VPC_NAME="My VPC"
 VPC_CIDR="10.0.0.0/16"
+SUBNET_PUBLIC_CIDR="10.0.1.0/24"
+SUBNET_PUBLIC_AZ="us-west-1a"
+SUBNET_PUBLIC_NAME="10.0.1.0 - us-west-1a"
 #==============================================================================
 #   DO NOT MODIFY CODE BELOW
 #==============================================================================
@@ -25,9 +28,23 @@ aws ec2 create-tags \
   --tags "Key=Name,Value=$VPC_NAME" \
   --region $AWS_REGION
 echo "  VPC ID '$VPC_ID' NAMED as '$VPC_NAME'."
-aws ec2 create-subnet `
-    --region "us-west-1" `
-    --availability-zone "us-west-1a" `
-    --vpc-id "vpc-03a5af3969245ddc4" `
-    --cidr-block "10.10.0.0/24" `
-    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=web}]"
+#create-subnet
+# Create Public Subnet
+echo "Creating Public Subnet..."
+SUBNET_PUBLIC_ID=$(aws ec2 create-subnet \
+  --vpc-id $VPC_ID \
+  --cidr-block $SUBNET_PUBLIC_CIDR \
+  --availability-zone $SUBNET_PUBLIC_AZ \
+  --query 'Subnet.{SubnetId:SubnetId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Subnet ID '$SUBNET_PUBLIC_ID' CREATED in '$SUBNET_PUBLIC_AZ'" \
+  "Availability Zone."
+
+# Add Name tag to Public Subnet
+aws ec2 create-tags \
+  --resources $SUBNET_PUBLIC_ID \
+  --tags "Key=Name,Value=$SUBNET_PUBLIC_NAME" \
+  --region $AWS_REGION
+echo "  Subnet ID '$SUBNET_PUBLIC_ID' NAMED as" \
+  "'$SUBNET_PUBLIC_NAME'."
